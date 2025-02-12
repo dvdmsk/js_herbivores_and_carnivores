e36f8d0a92cb9ce96f0d77153eae6893b746a141
David Moskalenko <dvdmsk21@gmail.com>
2025-02-12 23:07:40 +0200

    animal

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,46 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  health = 100;
+  static alive = [];
+  constructor(name) {
+    this.name = name;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+  bite(victim) {
+    if (victim.hidden) {
+      return;
+    }
+
+    if (victim instanceof Herbivore) {
+      victim.health -= 50;
+    }
+
+    if (victim.health <= 0) {
+      victim.health = 0;
+
+      const index = Animal.alive.indexOf(victim);
+
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
 
 module.exports = {
